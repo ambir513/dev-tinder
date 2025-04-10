0c72090db140445a112ea707cab5b5076ba84cf0
Amar Biradar <ambir0513@gmail.com>
2025-04-10 12:01:46 +0530

    Episode 02 - CURD Operation

--- src/app.js
+++ src/app.js
@@ -1,13 +1,76 @@
 const express = require("express");
 
 const app = express();
+app.use(express.json());
+let data = [];
 
-app.get("/", (req, res) => {
-  res.send("Hii this is Home page");
+app.get("/user/:userId", (req, res) => {
+  const { userId } = req.params;
+  const user = data.filter((user) => user.id == userId);
+
+  if (user.length === 0) {
+    return res.send("User is Not Found");
+  }
+  return res.send(user[0]);
 });
 
-app.get("/test", (req, res) => {
-  res.send("This is for testing!");
+app.post("/user", (req, res) => {
+  const { id, email, password } = req.body;
+  data.push({ id, email, password });
+
+  res.send("User Successfully Created");
+});
+
+app.delete("/user/:userId", (req, res) => {
+  const { userId } = req.params;
+  const user = data.filter((user) => user.id == userId);
+  if (user.length === 0) {
+    return res.send("User is Not found");
+  }
+  data = data.filter((user) => user.id != userId);
+  res.send(`${userId} User is Successfully Deleted`);
+});
+
+app.patch("/user/:userId", (req, res) => {
+  const { userId } = req.params;
+  const user = data.filter((user) => user.id == userId);
+  if (user.length === 0) {
+    return res.send("User is Not found");
+  }
+  const newUser = user.filter((user) => {
+    if (req.body.email) {
+      return (user.email = req.body.email);
+    } else if (req.body.password) {
+      return (user.password = req.body.password);
+    } else {
+      return (user.email = req.body.email), (user.password = req.body.password);
+    }
+  });
+  data = data.filter((user) => user.id != userId);
+  data.push(newUser[0]);
+  res.send("Successfully change");
+});
+
+app.put("/user/:userId", (req, res) => {
+  const { userId } = req.params;
+  const user = data.filter((user) => user.id == userId);
+  if (user.length === 0) {
+    return res.send("User is Not found");
+  }
+  const newUser = user.filter((user) => {
+    if (req.body.id && req.body.email && req.body.password) {
+      return (
+        (user.id = req.body.id),
+        (user.email = req.body.email),
+        (user.password = req.body.password)
+      );
+    } else {
+      return res.send("All fields are Mandatory");
+    }
+  });
+  data = data.filter((user) => user.id != userId);
+  data.push(newUser[0]);
+  res.send("Successfully change");
 });
 
 app.listen(7777, () => {
