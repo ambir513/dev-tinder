5d1899628cebdec7a84046e18d0a28ac2332cbc8
Amar Biradar <176784906+ambir513@users.noreply.github.com>
2025-05-10 16:26:23 +0530

    Update socket.js

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -10,7 +10,7 @@ const getHashAuth = (userId, _id) => {
 const initializeSocket = (server) => {
   const io = Socket(server, {
     cors: {
-      origin: "https://dev-tinder-ggrn.onrender.com",
+      origin: "https://devtinder.web.app",
     },
   });
 
